0d4a8931d243aaaf0250cef864e5e56917a110b6
david-peretz <david82761@gmail.com>
2025-04-22 15:59:47 +0300

    init

--- obj/Debug/net6.0/CustomerApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/CustomerApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd49bba18a418c8a8ffe261728c58dec4f5d3d2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
